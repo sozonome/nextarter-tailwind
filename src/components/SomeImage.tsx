d3a8956a4407e2cc87fb3bc4c0594db96e87e6ed
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-10-01 15:00:35 +0700

    chore: add img alt

--- src/components/SomeImage.tsx
+++ src/components/SomeImage.tsx
@@ -6,7 +6,12 @@ const SomeImage = () => {
   return (
     <>
       <div className="grid gap-2 text-center p-12">
-        <Image width={400} height={400} src="/assets/Website setup-amico.svg" />
+        <Image
+          width={400}
+          height={400}
+          src="/assets/Website setup-amico.svg"
+          alt="launch image"
+        />
 
         <AnchorLink href="https://storyset.com/web">
           Web illustrations by Storyset
