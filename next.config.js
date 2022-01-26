2db7ed7d0698a9357bb58ac9289c00003883cdc6
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-01-27 01:13:16 +0700

    chore: set eslint custom dirs next config

--- next.config.js
+++ next.config.js
@@ -7,9 +7,9 @@ module.exports = withPWA({
       process.env.NODE_ENV === "development" ||
       process.env.NODE_ENV === "preview" ||
       process.env.NODE_ENV === "production",
-      // delete two lines above to enable PWA in production deployment
-      // add your own icons to public/manifest.json 
-      // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
+    // delete two lines above to enable PWA in production deployment
+    // add your own icons to public/manifest.json
+    // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
     dest: "public",
     register: true,
   },
@@ -19,5 +19,8 @@ module.exports = withPWA({
   },
   experimental: {
     optimizeCss: true,
-  }
+  },
+  eslint: {
+    dirs: ["src"],
+  },
 });
