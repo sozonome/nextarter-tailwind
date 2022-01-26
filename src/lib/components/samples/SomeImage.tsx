e0a7b13cb95527e128bb5b4fdd10d4ace9c4fd60
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-01-27 01:36:45 +0700

    chore: update packages

--- src/lib/components/samples/SomeImage.tsx
+++ src/lib/components/samples/SomeImage.tsx
@@ -4,20 +4,18 @@ import AnchorLink from "lib/components/ui/AnchorLink";
 
 const SomeImage = () => {
   return (
-    <>
-      <div className="grid gap-2 p-12 text-center">
-        <Image
-          width={320}
-          height={320}
-          src="/assets/Website setup-amico.svg"
-          alt="launch image"
-        />
+    <div className="grid gap-2 p-12 text-center">
+      <Image
+        width={320}
+        height={320}
+        src="/assets/Website setup-amico.svg"
+        alt="launch image"
+      />
 
-        <AnchorLink href="https://storyset.com/web">
-          Web illustrations by Storyset
-        </AnchorLink>
-      </div>
-    </>
+      <AnchorLink href="https://storyset.com/web">
+        Web illustrations by Storyset
+      </AnchorLink>
+    </div>
   );
 };
 
