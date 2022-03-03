ceda953172488cb1e3302698f1df486f3e346651
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-03-03 17:10:35 +0700

    chore: format

--- src/lib/components/layout/ThemeSelect.tsx
+++ src/lib/components/layout/ThemeSelect.tsx
@@ -24,7 +24,7 @@ const themes = [
 
 const ThemeSelect = () => {
   return (
-    <select data-choose-theme className="select-bordered select">
+    <select data-choose-theme className="select select-bordered">
       {themes.map((theme) => (
         <option value={theme} key={theme}>
           {theme}
