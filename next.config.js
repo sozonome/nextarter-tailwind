7c0eac90747df78e06cb36ce92036589eceac7e2
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-05-03 13:22:34 +0700

    refactor(samples): revise CTAsection to not use next/image

--- next.config.js
+++ next.config.js
@@ -14,9 +14,6 @@ module.exports = withPWA({
     register: true,
   },
   reactStrictMode: true,
-  images: {
-    domains: ["vercel.com", "www.netlify.com"],
-  },
   experimental: {
     optimizeCss: true,
   },
--- src/lib/components/samples/CTASection.tsx
+++ src/lib/components/samples/CTASection.tsx
@@ -1,8 +1,35 @@
-import Image from "next/image";
-
+/* eslint-disable @next/next/no-img-element */
 const CTASection = () => {
   return (
-    <div className="grid gap-2.5">
+    <div className="grid justify-items-center gap-2.5">
+      <div className="flex items-center gap-2">
+        <a
+          href="https://vercel.com/import/git?s=https://github.com/sozonome/nextarter-tailwind"
+          className="flex items-center"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img
+            className="w-23 h-8"
+            src="https://vercel.com/button"
+            alt="Vercel deploy button"
+          />
+        </a>
+
+        <a
+          href="https://app.netlify.com/start/deploy?repository=https://github.com/sozonome/nextarter-tailwind"
+          className="flex items-center"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img
+            className="w-23 h-8"
+            src="https://www.netlify.com/img/deploy/button.svg"
+            alt="Netlify deploy button"
+          />
+        </a>
+      </div>
+
       <div className="flex items-center gap-2">
         <a
           className="btn rounded-3xl border-2 border-green-400 bg-gradient-to-br from-gray-100 to-green-200 font-semibold text-green-700 hover:from-gray-200 hover:to-green-200 hover:text-green-800"
@@ -12,21 +39,6 @@ const CTASection = () => {
         >
           Use This Template
         </a>
-
-        <a
-          href="https://vercel.com/import/git?s=https://github.com/sozonome/nextarter-tailwind"
-          className="flex items-center"
-        >
-          <Image
-            width={92}
-            height={32}
-            src="https://vercel.com/button"
-            alt="Vercel deploy button"
-          />
-        </a>
-      </div>
-
-      <div>
         <a
           className="btn rounded-3xl text-xs font-semibold"
           href="https://github.com/sozonome/nextarter-tailwind"
