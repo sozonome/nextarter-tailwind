f859d31cd1940ee5be1c3b6224ac9490456ae8cc
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-09-19 20:30:56 +0700

    chore: update next.config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,17 @@
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  disable:
+    process.env.NODE_ENV === "development" ||
+    process.env.NODE_ENV === "preview" ||
+    process.env.NODE_ENV === "production",
+  // delete two lines above to enable PWA in production deployment
+  // add your own icons to public/manifest.json
+  // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
+});
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
-  pwa: {
-    disable:
-      process.env.NODE_ENV === "development" ||
-      process.env.NODE_ENV === "preview" ||
-      process.env.NODE_ENV === "production",
-    // delete two lines above to enable PWA in production deployment
-    // add your own icons to public/manifest.json
-    // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
-    dest: "public",
-    register: true,
-  },
   reactStrictMode: true,
   experimental: {
     optimizeCss: true,
