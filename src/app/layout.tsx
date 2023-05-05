0a1e2d5a0b5577d9f8f03a675c66059744ff1f13
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-05-06 01:41:46 +0700

    chore: migrate to app router (#30)

---
+++ src/app/layout.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from 'next';
+
+import Layout from '@/lib/components/layout';
+import { fontSans } from '@/lib/styles/fonts';
+import { cn } from '@/lib/utils';
+
+import '@/lib/styles/globals.css';
+
+const APP_NAME = 'nextarter-tailwind';
+
+export const metadata: Metadata = {
+  title: APP_NAME,
+  description: 'Next.js App with TypeScript setup',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+  },
+  applicationName: APP_NAME,
+  appleWebApp: {
+    capable: true,
+    title: APP_NAME,
+    statusBarStyle: 'default',
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  themeColor: '#FFFFFF',
+};
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+const RootLayout = ({ children }: RootLayoutProps) => {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        <Layout>
+          <div className="flex-1">{children}</div>
+        </Layout>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
