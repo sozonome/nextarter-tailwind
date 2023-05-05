6f66e24b6d459863922d4ef06ec3f24c8d255077
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-05-06 01:40:18 +0700

    chore: migrate to app router

--- src/lib/components/layout/Header.tsx
+++ src/lib/components/layout/Header.tsx
@@ -1,17 +1,13 @@
-import Link from 'next/link';
-
 import { ThemeToggle } from '@/lib/components/theme-toggle';
 
 const Header = () => {
   return (
     <header className="sticky top-0 z-10 w-full bg-base-100/80 backdrop-blur-md">
       <section className="wrapper mx-auto flex items-center justify-between py-2">
-        <Link href="/" className="cursor-pointer text-lg font-bold">
+        <div className="ml-auto">
           <ThemeToggle />
-        </Link>
+        </div>
       </section>
-
-      <div className="h-1 bg-gradient-to-br from-primary to-accent" />
     </header>
   );
 };
--- src/lib/components/theme-toggle.tsx
+++ src/lib/components/theme-toggle.tsx
@@ -3,17 +3,20 @@
 import { useTheme } from 'next-themes';
 import { FaMoon, FaSun } from 'react-icons/fa';
 
+import { Button } from '@/lib/components/ui/button';
+
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme();
 
   return (
-    <button
-      type="button"
+    <Button
+      variant="ghost"
+      size="sm"
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
       <FaSun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <FaMoon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
-    </button>
+    </Button>
   );
 }
