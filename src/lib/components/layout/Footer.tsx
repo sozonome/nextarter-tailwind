93c303ed42c205f9538517bc984fd0bf603875bf
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-05-06 10:19:06 +0700

    refactor: update footer text size

--- src/lib/components/layout/Footer.tsx
+++ src/lib/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="wrapper">
       <div className="flex">
-        <p>
+        <p className="text-xs">
           {new Date().getFullYear()} -{' '}
           <a href="https://sznm.dev" target="_blank" rel="noopener noreferrer">
             sznm.dev
