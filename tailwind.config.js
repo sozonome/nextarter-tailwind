d3fd098ad130d39e0b27e317c62e82622417b708
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-07-05 02:51:29 +0700

    chore: setup shadcn ui components.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 module.exports = {
   darkMode: ['class'],
   content: ['src/**/*.{js,jsx,ts,tsx}'],
-
   theme: {
     container: {
       center: true,
