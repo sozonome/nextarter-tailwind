ed149314fe521c6b3a05d3449d447a12c4984769
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-11-21 14:31:02 +0700

    chore: increase contrast

--- src/lib/components/samples/SomeText.tsx
+++ src/lib/components/samples/SomeText.tsx
@@ -4,7 +4,7 @@ const SomeText = () => {
       <h1 className="bg-gradient-to-br from-gray-200 to-teal-700 bg-clip-text text-2xl font-bold text-transparent md:text-3xl">
         nextarter-tailwind
       </h1>
-      <p className="text-xs text-gray-500 md:text-sm">
+      <p className="text-xs md:text-sm">
         Next.js starter template with TailwindCSS and TypeScript setup
       </p>
     </div>
