f672b239f58a3bf2fb2fcaf3c2aa6077fb1ea297
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-12-27 23:02:01 +0700

    chore: convert tailwind config to typescript

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import tailwindTypography from '@tailwindcss/typography';
+import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
+export default {
+  darkMode: ['class'],
+  content: ['src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['var(--font-sans)', ...fontFamily.sans],
+    },
+  },
+  plugins: [tailwindTypography],
+} satisfies Config;
